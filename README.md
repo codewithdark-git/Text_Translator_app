138482c9d8c23e84ff4711a64f783adcb1dc431f
Codewithdark <144595403+codewithdark-git@users.noreply.github.com>
2024-03-03 11:44:50 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Text_Translator_app
+ This is a simple Streamlit web application that translates text from one language to another using the Google Translate API.
---
+++ app.py
@@ -0,0 +1,66 @@
+import streamlit as st
+from googletrans import LANGUAGES, Translator
+
+# Dictionary mapping language codes to full country names
+language_mapping = {code: LANGUAGES[code] for code in LANGUAGES}
+
+# List of all language codes
+all_language_codes = list(language_mapping.keys())
+
+# List of all language names
+all_language_names = list(language_mapping.values())
+
+# Customizing Streamlit's theme
+st.markdown(
+    """
+    <style>
+    /* Modify the background color */
+    body {
+        background-color: #f0f2f6;
+    }
+    /* Modify the text color */
+    .stApp {
+        color: white;
+    }
+    /* Increase font size */
+    .st-ec {
+        font-size: 30px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+def translate_text(text, target_language):
+    translator = Translator()
+    try:
+        translated_text = translator.translate(text, dest=target_language)
+        return translated_text.text
+    except Exception as e:
+        st.error(f"Translation failed: {e}")
+        return None
+
+def main():
+    st.title("Text Translator")
+
+    col1, col2 = st.columns([2, 1])
+
+    with col1:
+        input_text = st.text_area("Enter the text to translate:", height=200)
+
+    with col2:
+        target_language_name = st.selectbox("Select target language:", options=all_language_names)
+
+    target_language_code = [code for code, name in language_mapping.items() if name == target_language_name][0]
+
+    if st.button("Translate"):
+        if input_text and target_language_code:
+            translated_text = translate_text(input_text, target_language_code)
+            if translated_text is not None:
+                st.write(f"Translation to {target_language_name}:")
+                st.write(translated_text)
+        else:
+            st.warning("Please provide both text and select a target language.")
+
+if __name__ == "__main__":
+    main()
